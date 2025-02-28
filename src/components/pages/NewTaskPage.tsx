1c83af96310e42f73ec40144c15885a32ae8ea06
douglas <douglasmaxton@gmail.com>
2025-02-28 18:02:39 +0000

    feat(added): display items above input tag on entering

--- src/components/pages/NewTaskPage.tsx
+++ src/components/pages/NewTaskPage.tsx
@@ -3,26 +3,38 @@ import React, { useState } from "react";
 import PageHeader from "../organism/PageHeader.tsx";
 import Form from "../organism/Form";
 
+interface MyObject {
+  id: number;
+  title: string;
+  checked: boolean;
+}
+
 const NewTaskPage = () => {
   const [inputValue, setInputValue] = useState<string>("");
-  const handleCheckItems: { title: string; checked: boolean }[] = [];
+  // const handleCheckItems: { title: string; checked: boolean }[] = [];
+  const [handleCheckboxInput, setHandleCheckboxInput] = useState<MyObject[]>(
+    [],
+  );
 
-  console.log("handleCheckItems", handleCheckItems);
+  console.log("handleCheckboxInput", handleCheckboxInput);
 
   const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // only when enter is performed then save input data
     if (e.key === "Enter") {
       e.preventDefault();
+      setHandleCheckboxInput([
+        ...handleCheckboxInput,
+        {
+          id: handleCheckboxInput.length + 1,
+          title: inputValue,
+          checked: false,
+        },
+      ]);
       setInputValue("");
-      const task = {
-        title: inputValue,
-        checked: false,
-      };
-      handleCheckItems.push(task);
-      localStorage.setItem("checklist", JSON.stringify(handleCheckItems));
     }
   };
 
@@ -30,6 +42,10 @@ const NewTaskPage = () => {
     <div className="sm:w-120 md:w-150 lg:w-200">
       <PageHeader title="Add New Task" />
       <Form />
+
+      {handleCheckboxInput.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
 
       {/*checkbox*/}
       <div className={"relative mt-12 mb-5"}>

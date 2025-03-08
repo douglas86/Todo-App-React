30ccaf6d5e3e701f626bdd2adcbcb8464aa72245
douglas <douglasmaxton@gmail.com>
2025-03-08 11:59:30 +0000

    feat(added): logic to stop duplicate checkbox entries

--- src/components/pages/NewTaskPage.tsx
+++ src/components/pages/NewTaskPage.tsx
@@ -8,7 +8,11 @@ import useAppContext from "../../hooks/useAppContext.tsx";
 const NewTaskPage = () => {
   const { state, dispatch } = useAppContext();
 
+  const findItemInArray = (find: string) =>
+    state.checkboxReducer.items.indexOf(find) !== -1;
+
   console.log("state", state.checkboxReducer);
+  console.log("find", findItemInArray(state.checkboxReducer.input));
 
   const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: "CHECKBOX_INPUT", payload: e.target.value });
@@ -16,7 +20,7 @@ const NewTaskPage = () => {
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     // only when enter is performed then save input data
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !findItemInArray(state.checkboxReducer.input)) {
       e.preventDefault();
       dispatch({
         type: "ADD_CHECKBOX_INPUT",
@@ -46,6 +50,11 @@ const NewTaskPage = () => {
           onChange={handleSubmit}
           onKeyDown={handleKeyDown}
         />
+        {findItemInArray(state.checkboxReducer.input) && (
+          <p className={`text-red-400 font-bold text-center p-2`}>
+            Please add another item this has been added already!
+          </p>
+        )}
       </div>
     </div>
   );

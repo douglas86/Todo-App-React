d9a8adb4ccddf0a35e5887de583938b5d21bd755
douglas <douglasmaxton@gmail.com>
2025-04-05 13:55:23 +0100

    refactored: input forms with its own molecule

--- src/components/organism/Form.tsx
+++ src/components/organism/Form.tsx
@@ -1,6 +1,7 @@
 // import { ChangeEvent, useState } from "react";
 // import { useRedirect } from "react-admin";
 import { useForm, SubmitHandler } from "react-hook-form";
+import FormInputWithLevels from "../molecule/FormInputWithLevels.tsx";
 
 // import LevelSelection from "./LevelSelection.tsx";
 // import CheckboxList from "./CheckboxList.tsx";
@@ -119,23 +120,12 @@ const Form = () => {
   console.log("watch", watch("taskName"));
 
   return (
-    <form className="max-w-md mx-auto" onSubmit={handleSubmit(onSubmit)}>
-      <div className="relative z-0 w-full mb-5 group">
-        <input
-          type="text"
-          id="taskName"
-          className="block py-2.5 px-0 w-full text-sm text-green-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=" "
-          {...register("taskName", { required: true })}
-        />
-        <label
-          htmlFor="taskName"
-          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-        >
-          Task Name
-        </label>
-        {errors.taskName && <span>This Field is Required</span>}
-      </div>
+    <form className="max-w-md mx-auto m-5" onSubmit={handleSubmit(onSubmit)}>
+      <FormInputWithLevels
+        name={`taskname`}
+        reg={register("taskName", { required: true })}
+        error={errors.taskName}
+      />
 
       <button
         type="submit"

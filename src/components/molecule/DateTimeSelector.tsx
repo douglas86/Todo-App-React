8634351ad218e49cc3a7ee86aa0fb4db14a0fd37
douglas <douglasmaxton@gmail.com>
2025-04-12 17:08:57 +0100

    feat(added): label and styling to date time selector

--- src/components/molecule/DateTimeSelector.tsx
+++ src/components/molecule/DateTimeSelector.tsx
@@ -12,19 +12,25 @@ const DateTimeSelector = ({
   setState: UseFormSetValue<FormInputs>;
 }) => {
   return (
-    <DatePicker
-      selected={watch("date")}
-      dateFormat="d MMMM yyyy h:mmaa"
-      onChange={(d) => setState("date", d ? d : new Date())}
-      minDate={new Date()}
-      showTimeSelect
-      minTime={
-        isToday(watch("date"))
-          ? new Date()
-          : new Date(new Date().setHours(0, 0, 0, 0))
-      }
-      maxTime={new Date(new Date().setHours(23, 59, 59))}
-    />
+    <div className="flex flex-col items-center justify-center text-center">
+      <label className={` text-lg font-bold text-green-500`}>
+        Due Date and Time Selector
+      </label>
+      <DatePicker
+        selected={watch("date")}
+        dateFormat="d MMMM yyyy h:mm aa"
+        onChange={(d) => setState("date", d ? d : new Date())}
+        minDate={new Date()}
+        showTimeSelect
+        minTime={
+          isToday(watch("date"))
+            ? new Date()
+            : new Date(new Date().setHours(0, 0, 0, 0))
+        }
+        maxTime={new Date(new Date().setHours(23, 59, 59))}
+        className={`rounded-full m-4 text-center`}
+      />
+    </div>
   );
 };
 

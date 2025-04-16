defa96de45ca24fbf8449ceaf505e6712e6d954a
douglas <douglasmaxton@gmail.com>
2025-04-16 23:48:02 +0100

    feat(changed): redirect to home on the left arrow on the task header

--- src/components/organism/PageHeader.tsx
+++ src/components/organism/PageHeader.tsx
@@ -1,15 +1,17 @@
 import { roundButton } from "../atoms/buttons.tsx";
 import { leftArrowIcon } from "../atoms/icons/iconsFa.tsx";
 import { pageTitle } from "../atoms/elements.tsx";
+import { ChangeEvent } from "react";
 
 const PageHeader = ({ title }: { title: string }) => {
-  const handleClick = () => {
-    console.log("clicked");
+  const handleClick = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    window.location.href = "/";
   };
 
   return (
     <div className="flex">
-      <a href="/">{roundButton(10, leftArrowIcon(), () => handleClick())}</a>
+      <a href="/">{roundButton(10, leftArrowIcon(), () => handleClick)}</a>
       {pageTitle(title)}
     </div>
   );

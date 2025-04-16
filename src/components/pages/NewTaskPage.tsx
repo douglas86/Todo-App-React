117d334341432d3c4ef5633a015bfb93dc28b982
douglas <douglasmaxton@gmail.com>
2025-04-16 23:43:59 +0100

    feat(removed): creating session object in session storage

--- src/components/pages/NewTaskPage.tsx
+++ src/components/pages/NewTaskPage.tsx
@@ -1,14 +1,7 @@
 import PageHeader from "../organism/PageHeader.tsx";
 import Form from "../organism/Form";
-import { useEffect } from "react";
 
 const NewTaskPage = () => {
-  const session = localStorage.getItem("session");
-
-  useEffect(() => {
-    sessionStorage.setItem("session", JSON.stringify({}));
-  }, [session]);
-
   return (
     <div className="sm:w-120 md:w-150 lg:w-200">
       <PageHeader title="Add New Task" />

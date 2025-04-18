0887f5e7124d71a32df062ea6f1ee8291f1e0f91
douglas <douglasmaxton@gmail.com>
2025-04-18 19:22:32 +0100

    feat(added): iterating of data from local to timeline

--- src/components/organism/Timelined.tsx
+++ src/components/organism/Timelined.tsx
@@ -6,137 +6,81 @@ import {
   TimelineIcon,
   TimelineBody,
   Typography,
-  Avatar,
 } from "@material-tailwind/react";
-import { roundButton } from "../atoms/buttons.tsx";
 import { tickIcon } from "../atoms/icons/iconsTi.tsx";
+import useLocalStorage from "../../hooks/useLocalStorage.tsx";
+import {
+  checkPriorityLevel,
+  convertKeyToUsableData,
+  formatFriendlyDate,
+} from "../../utils/helpers.tsx";
+import { calendarIcon, upArrowIcon } from "../atoms/icons/iconsFa.tsx";
+import { complexIcon } from "../atoms/icons/iconsTb.tsx";
 
 const Timelined = () => {
-  // const { getLocal } = useLocalStorage();
-  //
-  // console.log("getLocal", getLocal());
+  const { getLocal } = useLocalStorage();
+
+  console.log("getLocal", getLocal());
 
   return (
-    <div className="w-[32rem]">
+    <div className="w-[32rem] p-3 my-5">
       <Timeline>
-        <TimelineItem>
-          <TimelineConnector className={`flex !w-1 bg-blue-500 ml-4.5`} />
-          <TimelineHeader>
-            <TimelineIcon className="p-0 bg-green-600">
-              <button
-                className={`flex w-10 h-10 justify-center items-center`}
-                onClick={() => console.log("clicked")}
-              >
-                {tickIcon}
-              </button>
-            </TimelineIcon>
-            <Typography variant="h5" color="blue-gray">
-              Timeline Title Here.
-            </Typography>
-          </TimelineHeader>
-          <TimelineBody className="pb-8">
-            <Typography color="gary" className="font-normal text-gray-600">
-              The key to more success is to have a lot of pillows. Put it this
-              way, it took me twenty five years to get these plants, twenty five
-              years of blood sweat and tears, and I&apos;m never giving up,
-              I&apos;m just getting started. I&apos;m up to something. Fan luv.
-            </Typography>
-          </TimelineBody>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineConnector className={`flex !w-1 bg-blue-500 ml-4`} />
-          <TimelineHeader>
-            <TimelineIcon className="p-0 bg-green-600">
-              <button
-                className={`flex w-10 h-10 justify-center items-center`}
-                onClick={() => console.log("clicked")}
-              >
-                {tickIcon}
-              </button>
-            </TimelineIcon>
-            <Typography variant="h5" color="blue-gray">
-              Timeline Title Here.
-            </Typography>
-          </TimelineHeader>
-          <TimelineBody className="pb-8">
-            <Typography color="gary" className="font-normal text-gray-600">
-              The key to more success is to have a lot of pillows. Put it this
-              way, it took me twenty five years to get these plants, twenty five
-              years of blood sweat and tears, and I&apos;m never giving up,
-              I&apos;m just getting started. I&apos;m up to something. Fan luv.
-            </Typography>
-          </TimelineBody>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineHeader>
-            <TimelineIcon className="p-0 bg-green-600">
-              <button
-                className={`flex w-10 h-10 justify-center items-center`}
-                onClick={() => console.log("clicked")}
-              >
-                {tickIcon}
-              </button>
-            </TimelineIcon>
-            <Typography variant="h5" color="blue-gray">
-              Timeline Title Here.
-            </Typography>
-          </TimelineHeader>
-          <TimelineBody>
-            <Typography color="gary" className="font-normal text-gray-600">
-              The key to more success is to have a lot of pillows. Put it this
-              way, it took me twenty five years to get these plants, twenty five
-              years of blood sweat and tears, and I&apos;m never giving up,
-              I&apos;m just getting started. I&apos;m up to something. Fan luv.
-            </Typography>
-          </TimelineBody>
-        </TimelineItem>
+        {Object.entries(getLocal()).map(([key, value]) => (
+          <TimelineItem key={key}>
+            <TimelineConnector className={`flex !w-1 bg-blue-500 ml-4.5`} />
+            <TimelineHeader>
+              <TimelineIcon className="p-0 bg-green-600">
+                <button
+                  className={`flex w-10 h-10 justify-center items-center`}
+                  onClick={() => console.log("clicked")}
+                >
+                  {tickIcon}
+                </button>
+              </TimelineIcon>
+              {/*timeline header*/}
+              <Typography variant="h5" color="blue-gray">
+                <div className={`flex`}>
+                  <div
+                    className={`mr-1 w-6 h-6 rounded-full ${formatFriendlyDate(value.date).backgroundTextColor}`}
+                  ></div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+                    {convertKeyToUsableData(key)}
+                  </h5>
+                </div>
+              </Typography>
+            </TimelineHeader>
+            <TimelineBody className="pb-8">
+              {/*show the date and time of a task*/}
+              <div className={`flex m-3`}>
+                <div className={`mr-2`}>{calendarIcon}</div>
+                <h3
+                  className={`text-lg ${formatFriendlyDate(value.date).forgroundTextColor}`}
+                >
+                  Due Date: {formatFriendlyDate(value.date).text}
+                </h3>
+              </div>
+              {/*priority levels*/}
+              <div className={`flex m-3`}>
+                <div className={`mr-2`}>{upArrowIcon}</div>
+                <h3 className={`text-lg text-gray-400`}>
+                  Priority: {checkPriorityLevel(value.priority)} (
+                  {value.priority}/10)
+                </h3>
+              </div>
+              {/*complex levels*/}
+              <div className={`flex m-3`}>
+                <div className={`mr-2`}>{complexIcon}</div>
+                <h3 className={`text-lg text-gray-400`}>
+                  Complexity: {checkPriorityLevel(value.complexity)} (
+                  {value.complexity}/10)
+                </h3>
+              </div>
+            </TimelineBody>
+          </TimelineItem>
+        ))}
       </Timeline>
     </div>
   );
 };
 
 export default Timelined;
-
-// const Timelined = () => {
-//   const { getLocal } = useLocalStorage();
-//
-//   console.log("getLocal", getLocal());
-//
-//   return (
-//     <div className="flex flex-col items-center justify-center p-5">
-//       <div className="relative border-l-3 border-gray-300 ml-4 space-y-12">
-//         {[
-//           {
-//             avatar: "/images/avatar1.jpg",
-//             title: "Timelined Title Here.",
-//             content: `The key to more success is to have a lot of pillows. Put it this way, it took me twenty five years to get these plants...`,
-//           },
-//           {
-//             avatar: "/images/avatar2.jpg",
-//             title: "Timelined Title Here.",
-//             content: `The key to more success is to have a lot of pillows...`,
-//           },
-//           {
-//             avatar: "/images/avatar3.jpg",
-//             title: "Timelined Title Here.",
-//             content: `I'm never giving up, I'm just getting started. I'm up to something. Fan luv.`,
-//           },
-//         ].map((item, index) => (
-//           <div key={index} className="relative pl-12">
-//             {/* Avatar */}
-//             <img
-//               src={item.avatar}
-//               alt="Avatar"
-//               className="w-10 h-10 rounded-full absolute left-[-20px] top-0 border-2 border-white shadow-md"
-//             />
-//             {/* Content */}
-//             <h3 className="text-lg font-semibold">{item.title}</h3>
-//             <p className="text-gray-600 text-sm">{item.content}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-//
-// export default Timelined;
